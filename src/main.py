bdaab975c8784121100fc2c82384dc4b9e039309
MegaTarasko <megatarasko@yandex.ru>
2024-01-10 14:12:36 +0300

    add command start

--- src/main.py
+++ src/main.py
@@ -3,6 +3,9 @@ import asyncio
 from dotenv import load_dotenv
 import os
 from utils.commands import set_commands
+from handlers.start import get_start
+from aiogram.filters import Command
+
 
 load_dotenv()
 
@@ -16,7 +19,7 @@ async def start_bot(bot: Bot):
     await bot.send_message(admin_id, text='hi')
 
 dp.startup.register(start_bot)
-
+dp.message.register(get_start, Command(commands='start'))
 
 async def start():
     await set_commands(bot)
