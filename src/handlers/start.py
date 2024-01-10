4bad3a91c5b9168266e5125e8518f0a7ef2ab1bd
MegaTarasko <megatarasko@yandex.ru>
2024-01-10 14:37:31 +0300

    add keyboard

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,7 +1,10 @@
 from aiogram import Bot
 from aiogram.types import Message
+from keyboards.register_kb import register_keyboard
+
+
 
 
 async def get_start(message: Message, bot: Bot):
     await bot.send_message(message.from_user.id, f'Здравствуйте, рад видеть Вас \n'
-                                                 f'Бот поможет вам')
+                                                 f'Бот поможет вам', reply_markup=register_keyboard)
