d5e20cb744aa805d02825f4ded84ab17a226f50c
MegaTarasko <megatarasko@yandex.ru>
2024-01-11 12:05:49 +0300

    fix answer -> send_message

--- src/handlers/register.py
+++ src/handlers/register.py
@@ -1,27 +1,28 @@
 from aiogram.types import Message
 from aiogram.fsm.context import FSMContext
+from aiogram import Bot
 from state.register import RegisterState
 import re
 import os
 from utils.database import Database
 
 
-async def start_register(message: Message, state: FSMContext):
+async def start_register(message: Message, state: FSMContext, bot: Bot):
     db = Database(os.getenv('DATABASE_NAME'))
     users = db.select_user_id(message.from_user.id)
     if (users):
-        await message.answer(f'Вы уже зарегистрированы под именем {users[1]}')
+        await bot.send_message(message.from_user.id, f'Вы уже зарегистрированы под именем {users[1]}')
     else:
         await (message.answer(f'Начнем регистрацию \n Как вас зовут?'))
         await state.set_state(RegisterState.regName)
 
-async def register_name(message: Message, state: FSMContext):
-    await message.answer(f'привет, пользователь {message.text} \n'
+async def register_name(message: Message, state: FSMContext, bot: Bot):
+    await bot.send_message(message.from_user.id, f'привет, пользователь {message.text} \n'
                         f'укажите номер телефона 📱')
     await state.update_data(regname= message.text)
     await state.set_state(RegisterState.regPhone)
 
-async def register_phone(message: Message, state: FSMContext):
+async def register_phone(message: Message, state: FSMContext, bot: Bot):
     if(re.findall('^\+?[7][-\(]?\d{3}\)?-?\d{3}-?\d{2}-?\d{2}$', message.text)):
         await state.update_data(regphone=message.text)
         reg_date = await state.get_data()
@@ -29,11 +30,11 @@ async def register_phone(message: Message, state: FSMContext):
         reg_phone = reg_date.get('regphone')
 
         msg = f'Регистрация успешна, {reg_name} \n\n Телефон - {reg_phone}'
-        await message.answer(msg)
+        await bot.send_message(message.from_user.id, msg)
         db = Database(os.getenv('DATABASE_NAME'))
         db.add_user(reg_name, reg_phone, message.from_user.id)
         await state.clear()
 
     else:
-        await message.answer(f'телефон неверен ❌')
+        await bot.send_message(message.from_user.id, f'телефон неверен ❌')
     
