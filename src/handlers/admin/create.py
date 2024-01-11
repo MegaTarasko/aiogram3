4ab09a9c536649bc8b23dccc3100a96db6f25c49
MegaTarasko <megatarasko@yandex.ru>
2024-01-11 13:12:57 +0300

    add check admin_id

---
+++ src/handlers/admin/create.py
@@ -0,0 +1,7 @@
+from  aiogram import Bot
+from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+
+
+async def create_game(message: Message, state: FSMContext, bot: Bot):
+    await bot.send_message(message.from_user.id, f'test')
