486b8a90dd4ec08c087bf77c0178618727283586
MegaTarasko <megatarasko@yandex.ru>
2024-01-14 10:06:14 +0300

    temp

--- src/Keyboards/create_kb.py
+++ src/Keyboards/create_kb.py
@@ -1,6 +1,7 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from utils.database import Database
-import os 
+import os
+import datetime
 
 def place_kb():
     db = Database(os.getenv('DATABASE_NAME'))
@@ -10,3 +11,10 @@ def place_kb():
         kb.button(text=f'{place[1]}', callback_data=f'{place[0]}')
     kb.adjust(1)
     return kb.as_markup()
+
+def date_kb():
+    kb = InlineKeyboardBuilder()
+    current_date = datetime.date.today()
+    for i in range(7):
+        current_date += datetime.timedelta(days=1)
+        kb.button(text=f"{current_date.strftime('%d.%m')}")
